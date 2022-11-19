4e93c2507381cc093733d50e035290b3cd507052
Pavel Griva <xturnerrx@gmail.com>
2022-11-19 21:20:36 +0300

    obnovlenie price stokov!

--- src/main/java/com/example/teletrader/repositories/StockRepository.java
+++ src/main/java/com/example/teletrader/repositories/StockRepository.java
@@ -2,9 +2,15 @@ package com.example.teletrader.repositories;
 
 import com.example.teletrader.models.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-    @Query("UPDATE Stock s SET s.price = random() * (100-50) + 50 WHERE s.id <> 0")
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE stocks SET price = random() * (100-50) + 50 WHERE id <> 0",
+    nativeQuery = true)
     void changePrice();
 }
