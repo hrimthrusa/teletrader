ee3d6dce8bf546b77b3bc69a212e016350bd55e8
Pavel Griva <xturnerrx@gmail.com>
2022-11-19 20:56:55 +0300

    mi blije

---
+++ src/main/java/com/example/teletrader/utilities/StockScheduler.java
@@ -0,0 +1,16 @@
+package com.example.teletrader.utilities;
+
+import com.example.teletrader.repositories.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StockScheduler {
+    private final StockRepository stockRepository;
+    @Scheduled(initialDelay = 1000L, fixedDelay = 5000L)
+    public void changePrice() {
+        stockRepository.changePrice();
+    }
+}
---
+++ src/main/resources/liquibase/db_changelog/filling_stocks_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO stocks (ticker, price)
+VALUES ('A', 157.8),
+       ('AAON', 157.8),
+       ('ASIX', 157.8),
+       ('ASLE', 157.8),
+       ('ASLN', 157.8),
+       ('ASM', 157.8),
+       ('ASMB', 157.8),
+       ('ASML', 157.8),
+       ('ASND', 157.8),
+       ('ASNS', 157.8),
+       ('ASO', 157.8);
