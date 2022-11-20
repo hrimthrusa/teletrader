0a751e7bf058b933d538f4ddd4bac55bfa93671f
Gorbunov Vladislav <wint-mil@mail.ru>
2022-11-20 12:28:36 +0300

    session from Entity manager

--- src/main/java/com/example/teletrader/repositories/SessionFactoryConfig.java
+++ src/main/java/com/example/teletrader/repositories/SessionFactoryConfig.java
@@ -10,48 +10,48 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration()
+//@Configuration()
 public class SessionFactoryConfig {
-    @Bean("dataSource")
-    public DataSource getDataSource() {
-        DataSourceBuilder b = DataSourceBuilder.create();
-        b.url("jdbc:postgresql://localhost:5432/teletrader");
-        b.driverClassName("org.postgresql.Driver");
-        b.username("postgres");
-        b.password("postgres");
-        return b.build();
-    }
-
-
-    //sessionfactory bean
-    @Bean(name="session")
+//    @Bean("dataSource")
+//    public DataSource getDataSource() {
+//        DataSourceBuilder b = DataSourceBuilder.create();
+//        b.url("jdbc:postgresql://localhost:5432/teletrader");
+//        b.driverClassName("org.postgresql.Driver");
+//        b.username("postgres");
+//        b.password("postgres");
+//        return b.build();
+//    }
+//
+//
+//    //sessionfactory bean
+//    @Bean(name="session")
+////    @Bean
+//    public SessionFactory getSessionFactory(DataSource dataSource) {
+//
+//        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+//
+//        builder.addProperties(getHibernateProperties());
+//        builder.scanPackages("ORM.Model");
+//
+//        return builder.buildSessionFactory();
+//    }
+//
+//    //Hibernate properties:
+//    private Properties getHibernateProperties() {
+//        Properties properties = new Properties();
+//
+//        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
+//        properties.put("hibernate.show_sql", "true");
+//        properties.put("hibernate.format_sql", "true");
+//        properties.put("hibernate.hbm2ddl.auto", "none");
+//
+//        return properties;
+//    }
+//
+//    //transaction manager bean
 //    @Bean
-    public SessionFactory getSessionFactory(DataSource dataSource) {
-
-        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
-
-        builder.addProperties(getHibernateProperties());
-        builder.scanPackages("ORM.Model");
-
-        return builder.buildSessionFactory();
-    }
-
-    //Hibernate properties:
-    private Properties getHibernateProperties() {
-        Properties properties = new Properties();
-
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.format_sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "none");
-
-        return properties;
-    }
-
-    //transaction manager bean
-    @Bean
-    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-        return transactionManager;
-    }
+//    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+//        return transactionManager;
+//    }
 }
